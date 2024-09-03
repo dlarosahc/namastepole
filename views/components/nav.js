3347bfcd203a2444560b43d65d03d743e21ea049
dlarosahc <darwinlarosahxc@gmail.com>
2024-09-02 20:23:48 -0400

    fav.icon

--- views/components/nav.js
+++ views/components/nav.js
@@ -1,17 +1,19 @@
 const navbar = document.querySelector('#navbar');
 
+
+
 const createNavHome = () => {
     navbar.innerHTML = `
-    <div class="max-w-7xl h-16 mx-auto flex items-center text-white px-4 justify-between">
+    <div class="max-w-7xl  h-16 mx-auto flex items-center  text-white px-4 justify-between">
             <p class=" font-bold text-xl ">Namaste Pole Fitness</p>
            
             <!-- Version Movil -->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12 md:hidden cursor-pointer p-2 hover:bg-purple-700 rounded-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12 lg:hidden cursor-pointer p-2 hover:ease-in hover:duration-300 rounded-lg">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
 
             <!-- Version de Escritorio -->
-            <div class="hidden md:flex flex-row gap-3 items-center">
+            <div class="hidden lg:flex flex-row gap-3 items-center">
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/">Home</a>
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/aboutus/">Nosotros</a>
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/prices/">Paquetes y Precios</a>
@@ -20,13 +22,13 @@ const createNavHome = () => {
             </div>
 
             <!-- Menu Mobile -->
-            <div class="text-white bg-slate-900/50 fixed top-16 right-0 left-0 bottom-0 hidden justify-center items-center flex-col gap-2  ">
-             <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/">Home</a>
-             <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/aboutus/">Nosotros</a>
-             <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/prices/">Paquetes y Precios</a>
-             <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/login/">Iniciar Sesión</a>
-             <a class="transition ease-in-out font-bold bg-purple-700 inline-block  px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0" href="/signup/">Registro</a>
-            </div>
+             <div class=" text-center fixed top-16 right-0   flex flex-col bg-violet-400/50 p-2  shadow-xl transition duration-500 ease-in-out transform -translate-y-96">
+      <a class="transition ease-in-out font-bold block mt-4 hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/">Home</a>
+      <a class="transition ease-in-out font-bold block mt-4 hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/aboutus/">Nosotros</a>
+      <a class="transition ease-in-out font-bold block mt-4 hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/prices/">Paquetes y Precios</a>
+      <a class="transition ease-in-out font-bold block mt-4 hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/login/">Iniciar Sesión</a>
+      <a class="transition ease-in-out font-bold bg-violet-700 inline-block px-4 py-2 leading-none border rounded text-white  hover:border-transparent hover:text-violet-400 hover:bg-white mt-4" href="/signup/">Registro</a>
+    </div>
         </div>
     `;
 };
@@ -42,7 +44,7 @@ const createNavSignup = () => {
             </svg>
 
             <!-- Version de Escritorio -->
-            <div class="hidden md:flex flex-row gap-3 items-center">
+            <div class="hidden lg:flex flex-row gap-3 items-center">
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/">Home</a>
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/login/">Nosotros</a>
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/login/">Paquetes y Precios</a>
@@ -71,7 +73,7 @@ const createNavLogin = () => {
             </svg>
 
             <!-- Version de Escritorio -->
-            <div class="hidden md:flex flex-row gap-3 items-center">
+            <div class="hidden lg:flex flex-row gap-3 items-center">
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/">Home</a>
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/aboutus/">Nosotros</a>
              <a class="transition ease-in-out font-bold block mt-4 lg:inline-block lg:mt-0  hover:bg-white px-4 py-2 hover:text-violet-400 rounded-lg" href="/login/">Paquetes y Precios</a>
@@ -139,14 +141,14 @@ navBtn.addEventListener('click', e => {
     if(!navBtn.classList.contains('active')){
         navBtn.classList.add('active');
         navBtn.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />'
-        menuMobile.classList.remove('hidden');
-        menuMobile.classList.add('flex');
+        menuMobile.classList.remove('-translate-y-96');
+        menuMobile.classList.add('translate-y-0');
 
     } else{
         navBtn.classList.remove('active');
     navBtn.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />';
-    menuMobile.classList.add('hidden');
-    menuMobile.classList.remove('flex');
+    menuMobile.classList.add('-translate-y-96');
+    menuMobile.classList.remove('translate-y-0');
 }
 });
 
